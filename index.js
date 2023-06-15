1552f03f581a8fee80ea466cd86228c951300b0f
runlaiyang <yangrunlai@vip.qq.com>
2023-06-15 16:21:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -44,5 +44,6 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`app listening at ${port}`);
-    console.log(`working in region ${process.env.AWS_REGION} on dynamodb table ${process.env.DYNAMODB_DEMO_TABLE}`)
+    console.log(`working in region ${process.env.AWS_REGION} on dynamodb table ${process.env.DYNAMODB_DEMO_TABLE}`);
+    process.exit(1);
 });
